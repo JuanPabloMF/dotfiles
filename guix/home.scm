8c3dd1bcba47eeb9c35d1f434f0330abeeb670fb
Juan Pablo Morales <juanpablomoralesf@gmail.com>
2025-03-11 16:45:44 -0300

    Added guix service and added a fix for claude3-7

--- guix/home.scm
+++ guix/home.scm
@@ -11,13 +11,14 @@
 	     (gnu packages haskell-xyz)
 	     (gnu packages python-xyz)
 	     (gnu packages texlive)
-	     (packages copilot)
 	     (guix transformations)
 	     (guix packages)
 	     (srfi srfi-1)
              (guix build-system emacs)
 	     (guix build-system gnu)
 	     (guix build-system copy)
+	     (packages copilot)
+	     (packages ensure-guix)
 	     )
 
 
@@ -30,94 +31,111 @@
 
 (define gptel-without-compilation
   (package
-    (inherit gptel-package)
-    (build-system copy-build-system)
-    (arguments
-     `(#:install-plan
-       '(("." "share/emacs/site-lisp/"))))))  ;; Copy the entire directory
+   (inherit gptel-package)
+   (build-system copy-build-system)
+   (arguments
+    `(#:install-plan
+      '(("." "share/emacs/site-lisp/"))))))  ;; Copy the entire directory
 
 (home-environment
- (packages (list emacs
-		 git
-                 emacs-ace-window
-                 emacs-all-the-icons
-                 emacs-atomic-chrome
-                 emacs-auto-yasnippet
-                 emacs-avy
-                 emacs-bug-hunter
-                 emacs-burly
-                 emacs-cape
-                 emacs-code-cells
-                 emacs-corfu
-                 emacs-counsel
-                 emacs-counsel-tramp
-                 emacs-dap-mode
-                 emacs-dash
-                 emacs-dashboard
-                 emacs-docker-tramp
-                 emacs-doom-modeline
-                 emacs-doom-themes
-                 emacs-easy-kill
-                 emacs-editorconfig
-                 emacs-elfeed
-                 emacs-eterm-256color
-                 emacs-evil-nerd-commenter
-                 emacs-exec-path-from-shell
-                 emacs-expand-region
-                 emacs-f
-                 emacs-flycheck
-                 emacs-fontaine
-                 emacs-general
-                 emacs-guix
-                 emacs-hide-lines
-                 emacs-hydra
-                 emacs-ivy
-                 emacs-ivy-rich
-                 emacs-ivy-yasnippet
-                 emacs-jsonrpc
-                 emacs-jupyter
-                 emacs-kind-icon
-                 emacs-lsp-ivy
-                 emacs-lsp-mode
-                 emacs-lsp-treemacs
-                 emacs-lsp-ui
-                 emacs-magit
-                 emacs-multiple-cursors
-                 emacs-no-littering
-                 emacs-org
-                 emacs-org-bullets
-                 emacs-org-chef
-                 emacs-org-download
-                 emacs-org-tree-slide
-                 emacs-ox-hugo
-                 emacs-page-break-lines
-                 emacs-perspective
-                 emacs-phi-search
-                 emacs-pretty-hydra
-                 emacs-projectile
-                 emacs-pyvenv
-                 emacs-restclient
-                 emacs-s
-                 emacs-sphinx-doc
-                 emacs-swiper
-                 emacs-transpose-frame
-                 emacs-use-package
-                 emacs-which-key
-                 emacs-yasnippet
-                 ;; font-aporetic
-                 font-iosevka
-                 font-iosevka-comfy
-                 node
-                 pandoc
-                 python
-                 python-jupytext
-                 python-lsp-server
-		 texlive
-		 emacs-transient
-		 emacs-copilot
-		 ;; emacs-gptel
-		 gptel-without-compilation
-		 ;; ((options->transformation
-		 ;;   '((with-commit . "emacs-gptel=2bb081e")))
-		 ;;  emacs-gptel))
-		 )))
+ (packages
+  (list emacs
+	git
+	emacs-use-package-ensure-guix
+        emacs-ace-window
+        emacs-all-the-icons
+        emacs-atomic-chrome
+        emacs-auto-yasnippet
+        emacs-avy
+        emacs-bug-hunter
+        emacs-burly
+        emacs-cape
+        emacs-code-cells
+        emacs-corfu
+        emacs-counsel
+        emacs-counsel-tramp
+        emacs-dap-mode
+        emacs-dash
+        emacs-dashboard
+        emacs-docker-tramp
+        emacs-doom-modeline
+        emacs-doom-themes
+        emacs-easy-kill
+        emacs-editorconfig
+        emacs-elfeed
+        emacs-eterm-256color
+        emacs-evil-nerd-commenter
+        emacs-exec-path-from-shell
+        emacs-expand-region
+        emacs-f
+        emacs-flycheck
+        emacs-fontaine
+        emacs-general
+        emacs-guix
+        emacs-hide-lines
+        emacs-hydra
+        emacs-ivy
+        emacs-ivy-rich
+        emacs-ivy-yasnippet
+        emacs-jsonrpc
+        emacs-jupyter
+        emacs-kind-icon
+        emacs-lsp-ivy
+        emacs-lsp-mode
+        emacs-lsp-treemacs
+        emacs-lsp-ui
+        emacs-magit
+        emacs-multiple-cursors
+        emacs-no-littering
+        emacs-org
+        emacs-org-bullets
+        emacs-org-chef
+        emacs-org-download
+        emacs-org-tree-slide
+        emacs-ox-hugo
+        emacs-page-break-lines
+        emacs-perspective
+        emacs-phi-search
+        emacs-pretty-hydra
+        emacs-projectile
+        emacs-pyvenv
+        emacs-restclient
+        emacs-s
+        emacs-sphinx-doc
+        emacs-swiper
+        emacs-transpose-frame
+        emacs-use-package
+	n                 emacs-which-key
+        emacs-yasnippet
+        ;; font-aporetic
+        font-iosevka
+        font-iosevka-comfy
+        node
+        pandoc
+        python
+        python-jupytext
+        python-lsp-server
+	texlive
+	emacs-transient
+	emacs-copilot
+	;; emacs-gptel
+	gptel-without-compilation
+	;; ((options->transformation
+	;;   '((with-commit . "emacs-gptel=2bb081e")))
+	;;  emacs-gptel))
+	))
+ (services
+  (list
+   (simple-service
+    'desktop-entries
+    home-desktop-entries-service-type
+    (list
+     (desktop-entyr
+      (name "Emacs")
+      (exec "bash -l -c 'emacs %F'")
+      (comment "GNU Emacs")
+      (type "Application")
+      (icon "emacs")
+      (categories '("Development" "TextEditor"))
+      (terminal #f))))))
+ )
